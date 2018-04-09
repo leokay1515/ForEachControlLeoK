511eea7d5121d68547a15726ad2abec8a9dc7d96
leokay1515 <36515738+leokay1515@users.noreply.github.com>
2018-04-09 13:01:41 -0400

    Initial commit

---
+++ ForEachControlLeoK/ForEachControlLeoKForm.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace ForEachControlLeoK
+{
+    partial class frmForEachControlLeoK
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblRandom = new System.Windows.Forms.Label();
+            this.lblWhy = new System.Windows.Forms.Label();
+            this.lstBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(188, 23);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(102, 34);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(47, 69);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(73, 20);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Leo Kay";
+            // 
+            // lblRandom
+            // 
+            this.lblRandom.AutoSize = true;
+            this.lblRandom.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRandom.Location = new System.Drawing.Point(76, 261);
+            this.lblRandom.Name = "lblRandom";
+            this.lblRandom.Size = new System.Drawing.Size(214, 20);
+            this.lblRandom.TabIndex = 2;
+            this.lblRandom.Text = "This label has no purpose";
+            // 
+            // lblWhy
+            // 
+            this.lblWhy.AutoSize = true;
+            this.lblWhy.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWhy.Location = new System.Drawing.Point(217, 188);
+            this.lblWhy.Name = "lblWhy";
+            this.lblWhy.Size = new System.Drawing.Size(43, 20);
+            this.lblWhy.TabIndex = 3;
+            this.lblWhy.Text = "Why";
+            // 
+            // lstBox
+            // 
+            this.lstBox.FormattingEnabled = true;
+            this.lstBox.Location = new System.Drawing.Point(30, 135);
+            this.lstBox.Name = "lstBox";
+            this.lstBox.Size = new System.Drawing.Size(120, 95);
+            this.lstBox.TabIndex = 4;
+            // 
+            // frmForEachControlLeoK
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(345, 299);
+            this.Controls.Add(this.lstBox);
+            this.Controls.Add(this.lblWhy);
+            this.Controls.Add(this.lblRandom);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmForEachControlLeoK";
+            this.Text = "For Each with Control Object by Leo Kay";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblRandom;
+        private System.Windows.Forms.Label lblWhy;
+        private System.Windows.Forms.ListBox lstBox;
+    }
+}
+
---
+++ ForEachControlLeoK/ForEachControlLeoKForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ForEachControlLeoK
+{
+    public partial class frmForEachControlLeoK : Form
+    {
+        public frmForEachControlLeoK()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //change the bkg color of all objects to gold
+            foreach (Control aControlObject in this.Controls)
+            {
+                aControlObject.BackColor = Color.Gold;
+
+                if (aControlObject.GetType() == typeof(Label))
+                {
+                    aControlObject.BackColor = Color.Silver;
+                }
+            }          
+        }
+    }
+}
